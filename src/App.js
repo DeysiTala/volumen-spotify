275da8f0c093e9a7ce4aef268b2fe97f9184b276
Deysi <sude.90@gmail.com>
2017-10-25 11:58:53 -0500

    ordenar container

--- src/App.js
+++ src/App.js
@@ -66,28 +66,56 @@ const SoundHelp = () =>{
   return(
     <div>
       <footer>
-   <span> <img src ="../descarga.jpg"/></span>
- 
- 
- 
-   <button type="button" class="btn btn-primary" data-toggle="modal" data-target=".bd-example-modal-sm">Small modal</button>
-   
-   <div class="modal fade bd-example-modal-sm" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true">
-     <div class="modal-dialog modal-sm">
-       <div class="modal-content">
-         ...
-       </div>
+   <div className="container su-windowssmall">
+     <div>
+     <span>Conectar a un dispositivo</span><a href="https://www.spotify.com/connect/?utm_source=wp-picker&utm_medium=web"><i class="fa fa-question-circle" aria-hidden="true"></i></a>
      </div>
-   </div>
- </footer>
- <a href="#miModal">Abrir Modal</a>
-<div id="miModal" class="modal">
-  <div class="modal-contenido">
-    <a href="#">X</a>
-    <h2>Mi primer Modal</h2>
-    <p>Este es mi primera ventana modal sin utilizar JavaScript.</p>
-  </div>  
+     <img src="../connect_header@1x.png"/>
+     <div>
+       <p>
+       Escucha y controla Spotify en todos tus dispositivos.
+       Arranca Spotify en otro dispositivo y por arte de magia aparecerá aquí.
+       </p>
+       </div>
+       <a className="btn btn-blue btn-small" href="https://www.spotify.com/connect/?utm_source=wp-picker&utm_medium=web" >
+        SABER MAS
+       </a>
+       !-- El botón para abrir el cuadro -->
+<button type="button" className="btn btn-primary"
+		data-toggle="modal" data-target="#miCuadroModal1">
+   
+  <div className="laptop"></div>
+  <div className="speaker"></div>
+ </button>
+
+
+<div id="miCuadroModal1" class="modal fade" role="dialog" tabindex="-1">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal">×</button>
+				<h4 class="modal-title">Cuadro modal #1</h4>
+			</div>
+			<div class="modal-body">
+				<p>
+					Esto ha sido el primer ejemplo de uso de un cuadro modal con bootstrap.
+				</p>
+				<p>
+					Un detalle importante es que en este ejemplo se define el cuadro en la propia página, en el HTML.
+					Aunque el usuario no lo vea su pantalla, el cuadro está ahí, incluso se podría ver su código HTML
+					con la opción de ver código fuente del navegador.
+				</p>
+			</div>
+			<div class="modal-footer">
+				<button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+			</div>
+		</div>
+	</div>
 </div>
+     </div>
+     
+ </footer>
+
 </div>
 
 
