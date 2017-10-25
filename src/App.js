afbc1f37fd6973a735d597798b8343e1ab38889a
carlinha talavera cardenas <sude.90@gmail.com>
2017-10-25 07:52:09 -0500

    modal editando

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import 'react-rangeslider/lib/index.css';
 
 
 
-class Horizontal extends Component {
+/*class Horizontal extends Component {
   constructor (props, context) {
     super(props, context)
     this.state = {
@@ -58,6 +58,41 @@ class Horizontal extends Component {
 }
 
 
-export default Horizontal;
+export default Horizontal;*/
 
 //<div className='value'>{value}</div>
+
+const SoundHelp = () =>{
+  return(
+    <div>
+      <footer>
+   <span> <img src ="../descarga.jpg"/></span>
+ 
+ 
+ 
+   <button type="button" class="btn btn-primary" data-toggle="modal" data-target=".bd-example-modal-sm">Small modal</button>
+   
+   <div class="modal fade bd-example-modal-sm" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true">
+     <div class="modal-dialog modal-sm">
+       <div class="modal-content">
+         ...
+       </div>
+     </div>
+   </div>
+ </footer>
+ <a href="#miModal">Abrir Modal</a>
+<div id="miModal" class="modal">
+  <div class="modal-contenido">
+    <a href="#">X</a>
+    <h2>Mi primer Modal</h2>
+    <p>Este es mi primera ventana modal sin utilizar JavaScript.</p>
+  </div>  
+</div>
+</div>
+
+
+
+  )
+}
+
+export default SoundHelp;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Horizontal from './App';
+import SoundHelp from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<Horizontal />, document.getElementById('root'));
+ReactDOM.render(<SoundHelp />, document.getElementById('root'));
 registerServiceWorker();
